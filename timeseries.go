1646dc6e2c400c8ebdfd8d33a1484c2c0077c5f1
Serge <s.borzykh@npo-echelon.ru>
2020-03-05 12:30:49 +0300

    added concurrent locks

--- timeseries.go
+++ timeseries.go
@@ -3,12 +3,14 @@ package timeseries
 import (
 	"fmt"
 	"math"
+	"sync"
 	"time"
 )
 
 type TimeSerieInt struct {
 	data         map[int64]int
 	quantization int64 // quantization interval in nanoseconds
+	lock         sync.RWMutex
 }
 
 // Creates new TimeSeriesInt, implementing integer time series
@@ -17,6 +19,7 @@ func NewTimeSeriesInt(quant time.Duration) *TimeSerieInt {
 	return &TimeSerieInt{
 		map[int64]int{},
 		quant.Nanoseconds(),
+		sync.RWMutex{},
 	}
 }
 
@@ -27,7 +30,9 @@ func (ts *TimeSerieInt) AvgPerSecond(from, to time.Time) int {
 // Adds value to serie, time rounds to quant duration
 func (ts *TimeSerieInt) Add(t time.Time, value int) {
 	timeInt64 := ts.GetRoundedUnixTime(t)
+	ts.lock.Lock()
 	ts.data[timeInt64] = ts.data[timeInt64] + value
+	ts.lock.Unlock()
 }
 
 // returns average per quant value
@@ -50,9 +55,11 @@ func (ts *TimeSerieInt) Sum(from, to time.Time) int {
 		return -1
 	}
 	var sum = 0
+	ts.lock.RLock()
 	for i := fromUnix; i < toUnix; i += ts.quantization {
 		sum += ts.data[i]
 	}
+	ts.lock.RUnlock()
 	return sum
 }
 
@@ -64,10 +71,12 @@ func (ts *TimeSerieInt) GetIntervalSerieSlice(from, to time.Time) (serie []int) 
 	}
 	serie = make([]int, (toUnix-fromUnix)/ts.quantization)
 	i2 := 0
+	ts.lock.RLock()
 	for i := fromUnix; i < toUnix; i += ts.quantization {
 		serie[i2] = ts.data[i]
 		i2++
 	}
+	ts.lock.RUnlock()
 	return
 }
 
@@ -78,17 +87,20 @@ func (ts *TimeSerieInt) GetIntervalSerieMap(from, to time.Time) (serie map[time.
 		return
 	}
 	serie = map[time.Time]int{}
+	ts.lock.RLock()
 	for i := fromUnix; i < toUnix; i += ts.quantization {
 		if value, ok := ts.data[i]; ok {
 			serie[time.Unix(0, i)] = value
 		}
 	}
+	ts.lock.RUnlock()
 	return
 }
 
 func (ts *TimeSerieInt) FitstLastTimeTime() (first, last time.Time) {
 	var minTime int64 = math.MaxInt64
 	var maxTime int64 = 0
+	ts.lock.RLock()
 	for t := range ts.data {
 		if minTime > t {
 			minTime = t
@@ -97,22 +109,26 @@ func (ts *TimeSerieInt) FitstLastTimeTime() (first, last time.Time) {
 			maxTime = t
 		}
 	}
+	ts.lock.RUnlock()
 	return time.Unix(0, minTime), time.Unix(0, maxTime)
 }
 
 func (ts *TimeSerieInt) ClearInterval(from, to time.Time) {
 	fromUnix := ts.GetRoundedUnixTime(from)
 	toUnix := ts.GetRoundedUnixTime(to)
+	ts.lock.Lock()
 	for i := fromUnix; i < toUnix; i += ts.quantization {
 		if _, ok := ts.data[i]; ok {
 			delete(ts.data, i)
 		}
 	}
+	ts.lock.Unlock()
 }
 
 func (ts *TimeSerieInt) PrettyPrint(showZeroes bool) {
 	first, last := ts.FitstLastTimeTime()
 	fmt.Println("[")
+	ts.lock.RLock()
 	for {
 		if first.After(last) {
 			break
@@ -123,12 +139,11 @@ func (ts *TimeSerieInt) PrettyPrint(showZeroes bool) {
 		}
 		first = first.Add(time.Duration(ts.quantization))
 	}
-
+	ts.lock.Unlock()
 	fmt.Println("]")
 }
 
 func (ts *TimeSerieInt) GetRoundedUnixTime(t time.Time) int64 {
-	//unixNano := (t.Unix()*1000000000 + t.UnixNano())
 	unixNano := t.UnixNano()
 	return unixNano - (unixNano % ts.quantization)
 }
